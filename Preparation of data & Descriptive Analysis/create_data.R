289291f5fa3411155c536c9d01a1aab2e1afc0c9
Gorrd <gorrd34@gmail.com>
2015-12-08 23:24:02 +0100

    Changement encodage

--- Preparation of data & Descriptive Analysis/create_data.R
+++ Preparation of data & Descriptive Analysis/create_data.R
@@ -1,12 +1,12 @@
 #######################################################################################
 #                                                                                     #
-#                       Lecture et prÃ©paration des donnÃ©es                            #
+#                       Lecture et préparation des données                            #
 #                                                                                     #
 #######################################################################################
 
-# Lecture des donnÃ©es et transformation prÃ©alables des variables quantitatives
+# Lecture des données et transformation préalables des variables quantitatives
 
-# Importation des donnÃ©es
+# Importation des données
 ozone <- read.table('ozone.dat',h=T)
 
 # Changement du type de la variable jour
@@ -17,7 +17,7 @@ par(mfrow=c(2,4))
 hist(ozone[,"O3obs"]);hist(ozone[,"MOCAGE"]);hist(ozone[,"TEMPE"]);hist(ozone[,"RMH2O"]);
 hist(ozone[,"NO2"]);hist(ozone[,"NO"]);hist(ozone[,"VentMOD"]);hist(ozone[,"VentANG"]);
 
-# Transformation des variables et tracÃ© des histogrammes aprÃ¨s transformation
+# Transformation des variables et tracé des histogrammes après transformation
 ozone[,"SRMH2O"]=sqrt(ozone[,"RMH2O"])
 ozone[,"LNO2"]=log(ozone[,"NO2"])
 ozone[,"LNO"]=log(ozone[,"NO"])
@@ -26,7 +26,7 @@ par(mfrow=c(2,4))
 hist(ozone[,"O3obs"]);hist(ozone[,"MOCAGE"]);hist(ozone[,"TEMPE"]);hist(ozone[,"SRMH2O"]);
 hist(ozone[,"LNO2"]);hist(ozone[,"LNO"]);hist(ozone[,"VentMOD"]);hist(ozone[,"VentANG"]);
 
-# Suppression des variables inutiles pour la suite et crÃ©ation de la variable rÃ©ponse 
-# (variable binaire Ã  expliquer): DepSeuil
+# Suppression des variables inutiles pour la suite et création de la variable réponse 
+# (variable binaire à expliquer): DepSeuil
 ozone=ozone[,c(1:4,8:13)]
 ozone[,"DepSeuil"]=as.factor(ozone[,"O3obs"]>150)
